7c0672853232d0590b6edf41baffbad38a9c60a8
Stroh Snow <82967876+strohsnow@users.noreply.github.com>
2025-11-04 00:44:09 +0300

    Update iptables.sh

--- scripts/iptables.sh
+++ scripts/iptables.sh
@@ -14,6 +14,7 @@ iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
 iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 
+iptables -A INPUT -i tailscale0 -p tcp --dport 2222 -j ACCEPT  # Only on workers
 iptables -A INPUT -i tailscale0 -p tcp --dport 2377 -j ACCEPT  # Only on manager
 iptables -A INPUT -i tailscale0 -p tcp --dport 7946 -j ACCEPT
 iptables -A INPUT -i tailscale0 -p udp --dport 7946 -j ACCEPT
