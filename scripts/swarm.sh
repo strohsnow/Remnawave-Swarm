eb648952b199080e44cd557f8b0481869de93c32
Stroh Snow <82967876+strohsnow@users.noreply.github.com>
2025-11-05 00:39:35 +0300

    Update swarm.sh

--- scripts/swarm.sh
+++ scripts/swarm.sh
@@ -3,8 +3,8 @@
 tailscale down && tailscale up
 TAILSCALE_IP="$(tailscale ip -4)"
 
-docker swarm init --advertise-addr "${TAILSCALE_IP}"
+docker swarm init --advertise-addr "${TAILSCALE_IP}" > /dev/null
+docker swarm join-token manager
 
 docker network create -d overlay edge-network
 docker network create -d overlay remnawave-network
-
