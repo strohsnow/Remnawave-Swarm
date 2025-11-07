726fd489c2a303632e462a3657a0d03a3522c7cf
Stroh Snow <strohsnow@gmail.com>
2025-11-07 16:48:44 +0300

    update logrotate script

--- scripts/logrotate.sh
+++ scripts/logrotate.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+sudo install -d -m 755 /var/log/remnanode
+
+sudo install -d -m 755 /etc/logrotate.d
 sudo tee /etc/logrotate.d/remnanode > /dev/null << "EOF"
 /var/log/remnanode/*.log {
   size 50M
